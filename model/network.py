d1c746672266a5d3e4ebaefd2cd6f5299c204ea9
Remomare <remomare@naver.com>
2024-04-22 05:50:29 +0900

    04220550

--- model/network.py
+++ model/network.py
@@ -2,7 +2,10 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 
+from functools import partial
+
 from model import layers
+from model import utility
 
 class Unet_DDPM(nn.Module):
     def __init__(self,
@@ -27,7 +30,84 @@ class Unet_DDPM(nn.Module):
         
         self.channels = channels
         self.self_condition = self_condition
+        input_channels = channels * (2 if self_condition else  1)
         
+        int_dim = utility.default(init_dim, dim)
+        self.init_conv = layers.BasicConv(input_channels, init_dim, 7)
+        
+        dims = [init_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+        
+        block_klass = utility.partial(layers.ResBlock ,groups = resnet_block_groups)
+        
+        time_dim = dim * 4
+        
+        self.random_or_learned_sinusoidal_cond = learned_sinusoidal_cond or random_fourier_features
+        
+        if self.random_or_learned_sinusoidal_cond:
+            sinu_pos_emb = layers.Random_or_Learned_Sinusoidal_Positional_Embed(learned_sinusoidal_dim, random_fourier_features)
+            fourier_dim = learned_sinusoidal_dim + 1
+        else:
+            sinu_pos_emb = layers.Sinusoidal_Positional_Embed(dim, theta= sinusoidal_pos_emb_theta)
+            fourier_dim = dim
+        
+        self.time_mlp = nn.Sequential(
+            sinu_pos_emb,
+            nn.Linear(fourier_dim, time_dim),
+            nn.GELU(),
+            nn.Linear(time_dim, time_dim)
+        )
+        
+        if not full_attn:
+            full_attn = (*((False,) * (len(dim_mults) - 1)), True)
+            
+        num_stages = len(dim_mults)
+        full_attn = utility.cast_tuple(full_attn, num_stages)
+        attn_heads = utility.cast_tuple(attn_heads, num_stages)
+        attn_dim_head = utility.cast_tuple(attn_dim_head, num_stages)
+        
+        assert len(full_attn) == len(dim_mults)
+        
+        FullAttention = partial(layers.Attention, flash = flash_attn)
+        
+        self.down = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out)
+        
+        for ind, ((dim_in, dim_out), layer_full_attn, layer_attn_heads, layer_attn_dim_head) in enumerate(zip(in_out, full_attn, attn_heads, attn_dim_head)):
+            is_last = ind >= (num_resolutions - 1)
+
+            attn_klass = FullAttention if layer_full_attn else layers.LinearAttention
+
+            self.downs.append(nn.ModuleList([
+                block_klass(dim_in, dim_in, time_emb_dim = time_dim),
+                block_klass(dim_in, dim_in, time_emb_dim = time_dim),
+                attn_klass(dim_in, dim_head = layer_attn_dim_head, heads = layer_attn_heads),
+                layers.Downsample(dim_in, dim_out) if not is_last else nn.Conv2d(dim_in, dim_out, 3, padding = 1)
+            ]))
+
+        mid_dim = dims[-1]
+        self.mid_block1 = block_klass(mid_dim, mid_dim, time_emb_dim = time_dim)
+        self.mid_attn = FullAttention(mid_dim, heads = attn_heads[-1], dim_head = attn_dim_head[-1])
+        self.mid_block2 = block_klass(mid_dim, mid_dim, time_emb_dim = time_dim)
+
+        for ind, ((dim_in, dim_out), layer_full_attn, layer_attn_heads, layer_attn_dim_head) in enumerate(zip(*map(reversed, (in_out, full_attn, attn_heads, attn_dim_head)))):
+            is_last = ind == (len(in_out) - 1)
+
+            attn_klass = FullAttention if layer_full_attn else layers.LinearAttention
+
+            self.ups.append(nn.ModuleList([
+                block_klass(dim_out + dim_in, dim_out, time_emb_dim = time_dim),
+                block_klass(dim_out + dim_in, dim_out, time_emb_dim = time_dim),
+                attn_klass(dim_out, dim_head = layer_attn_dim_head, heads = layer_attn_heads),
+                layers.Upsample(dim_out, dim_in) if not is_last else  nn.Conv2d(dim_out, dim_in, 3, padding = 1)
+            ]))
+
+        default_out_dim = channels * (1 if not learned_variance else 2)
+        self.out_dim = utility.default(out_dim, default_out_dim)
+
+        self.final_res_block = block_klass(dim * 2, dim, time_emb_dim = time_dim)
+        self.final_conv = nn.Conv2d(dim, self.out_dim, 1)
 
 
 class Guidance(nn.Module):
